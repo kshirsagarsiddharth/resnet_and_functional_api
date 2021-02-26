484d9c2fc584a292cc6bd8801e4b205fdc3226a6
kshirsagarsiddharth <kshirsagarsiddharth45@gmail.com>
2021-02-26 16:31:34 +0530

    trained the model

--- happy_sad_keras/happy_sad_detector_keras.py
+++ happy_sad_keras/happy_sad_detector_keras.py
@@ -5,6 +5,15 @@ import pandas as pd
 import seaborn as sns 
 import matplotlib.pyplot as plt 
 import h5py
+import numpy as np
+from tensorflow.keras import layers
+from tensorflow.keras.layers import Input, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D
+from tensorflow.keras.layers import AveragePooling2D, MaxPooling2D, Dropout, GlobalMaxPooling2D, GlobalAveragePooling2D
+from tensorflow.keras.models import Model
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.utils import layer_utils
+from tensorflow.keras.utils.data_utils import get_file
+from tensorflow.keras.applications.imagenet_utils import preprocess_input
 #######################################################################################################
 
 def load_dataset():
@@ -67,4 +76,73 @@ def model(input_shape):
     return model 
 
 
+# GRADED FUNCTION: HappyModel
 
+def HappyModel(input_shape):
+    """
+    Implementation of the HappyModel.
+    
+    Arguments:
+    input_shape -- shape of the images of the dataset
+        (height, width, channels) as a tuple.  
+        Note that this does not include the 'batch' as a dimension.
+        If you have a batch like 'X_train', 
+        then you can provide the input_shape using
+        X_train.shape[1:]
+    
+
+    Returns:
+    model -- a Model() instance in Keras
+ """
+    
+    ### START CODE HERE ###
+    # Feel free to use the suggested outline in the text above to get started, and run through the whole
+    # exercise (including the later portions of this notebook) once. The come back also try out other
+    # network architectures as well. 
+    
+    X_input = Input(input_shape) 
+    
+    X = ZeroPadding2D((3,3))(X_input) 
+    X = Conv2D(32, (7,7), name = 'conv0')(X) 
+    X = BatchNormalization(axis=3, name = "bn0")(X) 
+    X = Activation('relu')(X)
+    
+    X = MaxPooling2D(name = 'max_pool1')(X)
+    
+    X = ZeroPadding2D((3,3))(X) 
+    X = Conv2D(64, (7,7), name = 'conv1')(X) 
+    X = BatchNormalization(axis=3, name = "bn1")(X) 
+    X = Activation('relu')(X) 
+    
+    X = MaxPooling2D(name = 'mp2')(X)
+    
+    X = Flatten()(X) 
+    X = Dense(16, activation='relu')(X)
+    X = Dropout(0.3)(X)
+    X = Dense(1, activation='sigmoid', name = 'fc')(X)
+
+    
+    model = Model(inputs = X_input, outputs = X, name = 'HappyModel')
+    ### END CODE HERE ###
+    
+    return model
+
+### START CODE HERE ### (1 line)
+happyModel = HappyModel(X_train.shape[1:])
+### END CODE HERE ###
+### START CODE HERE ### (1 line)
+happyModel.compile(loss='binary_crossentropy',optimizer='adam',metrics = ["accuracy"])
+### END CODE HERE ###
+
+
+### START CODE HERE ### (1 line)
+happyModel.fit(X_train,Y_train,batch_size = 128,epochs = 300, validation_split=0.1)
+### END CODE HERE ###
+
+### START CODE HERE ### (1 line)
+preds = happyModel.evaluate(X_test,Y_test)
+### END CODE HERE ###
+print()
+print ("Loss = " + str(preds[0]))
+print ("Test Accuracy = " + str(preds[1]))
+# %%
